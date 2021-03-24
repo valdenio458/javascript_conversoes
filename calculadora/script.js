dc576b0016531cdf5786cd03f7d65c6df610e793
Valdênio Martinho Pereira <valdeniomartinho@gmail.com>
2021-03-24 08:15:43 -0300

    ajustes

--- calculadora/script.js
+++ calculadora/script.js
@@ -4,53 +4,56 @@ console.log(primeiroValor)
 var segundoValor = document.getElementById("n2").value;
 console.log(segundoValor)
 var operacao = document.getElementById("op").value;
-console.log(operacao)
+console.log(typeof(operacao))
 var res = document.getElementById('res');
 
 // Opção usando if, else if e else:
+
 if(operacao == 1){
   var resultado = parseInt(primeiroValor) + parseInt(segundoValor); 
-  res.innerHTML =  resultado ;  
+  res.innerHTML =  resultado;  
   }else if(operacao == 2){
   var resultado = primeiroValor - segundoValor;
-  res.innerHTML =  resultado ; 
+  res.innerHTML =  resultado; 
 }else if(operacao == 3){
   var resultado = primeiroValor * segundoValor;
-  res.innerHTML =  resultado ; 
+  res.innerHTML =  resultado; 
 }else if(operacao == 4){
   var resultado = parseFloat(primeiroValor) / parseFloat(segundoValor);
-  res.innerHTML =  resultado ; 
+  res.innerHTML =  resultado; 
 }else{
   res.innerHTML = "Opção inválida!"
 }
 
 }
-
+/*
 // Opção usando switch:
 
-/*switch(operacao){
-  case 1:
-    var resultado = primeiroValor + segundoValor;    
-    document.write("<h2>" + resultado + "</h2>");
+switch(operacao){
+  case 1:          
+    var resultado = parseInt(primeiroValor) + parseInt(segundoValor); 
+    res.innerHTML =  resultado; 
+    
     break;
   
   case 2:
     var resultado = primeiroValor - segundoValor;    
-    document.write("<h2>" + resultado + "</h2>");
+    res.innerHTML =  resultado; 
     break;
 
   case 3:
     var resultado = primeiroValor * segundoValor;    
-    document.write("<h2>" + resultado + "</h2>");
+    res.innerHTML =  resultado; 
     break;
   
   case 4:
     var resultado = primeiroValor / segundoValor;    
-    document.write("<h2>" + resultado + "</h2>");
+    res.innerHTML =  resultado; 
     break;
 
   default:
-    document.write("<h2>Opção Inválida!</h2>")
+    res.innerHTML = "Opção inválida!"
 
+  }
 }
 */
